46e77958dfa49abac421863047c0c1468c50e86a
Karinaislam <badhon12karina@gmail.com>
2017-12-21 10:57:49 -0600

    4th

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyArLjyvUGb79MyQyPxDDICWTTd0w5pRGXo';
 
 
@@ -13,18 +14,27 @@ const API_KEY = 'AIzaSyArLjyvUGb79MyQyPxDDICWTTd0w5pRGXo';
 class App extends Component {
    constructor(props) {
        super(props);
-       this.state={videos:[]};
+       this.state={
+           videos:[],
+        selectedVideo:null
+    };
 
        YTSearch ({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({videos}); // this.setState({videos(key):videos(value)})
+        this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+            }); // this.setState({videos(key):videos(value)})
     });
    }
-    
+
     render() {
         return (
             <div>
                  <SearchBar />
-                 <VideoList videos={this.state.videos}/>
+                 <VideoDetail video={this.state.selectedVideo} />
+                 <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
